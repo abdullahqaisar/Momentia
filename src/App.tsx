1d92c0dfb573c07a3fcf927e212f7715d6fec1cc
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-06-08 16:55:39 +0500

    feat: Add react-router-dom for routing and update App component
    The code changes include:
    - Importing `BrowserRouter` from `react-router-dom` in `main.tsx`
    - Adding routes and components for sign-in, sign-up, and home pages in `App.tsx`
    - Updating `package.json` to include `react-router-dom` as a dependency
    - Updating `tailwind.config.js` to add custom colors and screen sizes
    
    This commit adds the necessary changes to implement routing using `react-router-dom` and update the `App` component to include the new routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import {Routes, Route} from "react-router-dom"
+
 import './globals.css'
+import SigninForm from "./_auth/forms/SigninForm"
+import { Home } from "./_root/pages"
+import SignupForm from "./_auth/forms/SignupForm"
+import RootLayout from "./_root/RootLayout"
+import AuthLayout from "./_auth/AuthLayout"
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="flex h-screen">
+      {/*Public routes */}
+      <Routes>
+        <Route element={<AuthLayout/>}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/*Priavte routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
