8e1cbcc95c3db737228bfde81395465c2cb04a87
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-06-08 23:44:56 +0500

    feat: Add utility function for merging CSS classes
    This commit introduces a new utility function `cn` in the `utils.ts` file. The function uses the `clsx` and `twMerge` libraries to merge multiple CSS class values into a single string. This will help simplify the process of applying multiple classes to elements in the codebase.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+  const isAuthenticated = false;
 
-export default AuthLayout
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
