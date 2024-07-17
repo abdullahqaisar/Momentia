fe9f675c5669628b73f320292be3b3b9c566ea14
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-07-17 19:53:16 +0500

    fix: Fix type

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -36,7 +36,7 @@ const Topbar = () => {
           <Link to={`/profile/${user.id}`} className="flex-center gap-3">
             <img
               src={user.imageUrl || `/assets/icons/profile-placeholder.svg`}
-              alt="profiel"
+              alt="profile"
               className="h-8 w-8 rounded-full"
             />
           </Link>
