021dbee0c6660d82a49b81a6404dd1ba815c08c4
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-07-27 14:29:13 +0500

    feat: Add new pages and routes
    This commit adds new pages and routes for the following features:
    - Explore
    - Saved
    - AllUsers
    - CreatePost
    - EditPost
    - PostDetails
    - Profile
    - UpdateProfile
    
    These pages are added to the existing `App` component and are accessible through the corresponding routes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import SignupForm from "./_auth/forms/SignupForm";
 import RootLayout from "./_root/RootLayout";
 import AuthLayout from "./_auth/AuthLayout";
@@ -21,6 +31,14 @@ const App = () => {
         {/*Priavte routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id/*" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
