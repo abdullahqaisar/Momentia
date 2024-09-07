6f445e4e36083ca520ab4e08dc2a864b68b7e88e
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-09-07 14:13:55 +0500

    feat: add changes for edit post and post stats

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -11,6 +11,8 @@ import {
   createPost,
   createUserAccount,
   deleteSavedPost,
+  getCurrentUser,
+  getPostById,
   getRecentPosts,
   likePost,
   savePost,
@@ -121,5 +123,20 @@ export const useDeleteSavedPost = () => {
         queryKey: [QUERY_KEYS.GET_CURRENT_USER],
       });
     },
+  });
+};
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser,
+  });
+};
+
+export const useGetPostById = (postId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+    queryFn: () => getPostById(postId),
+    enabled: !!postId,
   });
 };
